f0cbad89ca0a695950a625b0323455c314d8880a
AkshayKumar2211 <taara0361@gmail.com>
2025-05-22 22:57:55 +0530

    /folder

---
+++ tryCode/learn.js
@@ -0,0 +1,14 @@
+"use strict"; //this code will strictly follow the newer version of javascript
+let nu=null;
+console.log(typeof nu);
+
+//typeof is used to find the datatype of the variable
+let conversion="333";
+
+let Convert=Number(conversion);
+
+console.log(typeof Convert);
+
+Convert=String(Convert);
+
+console.log(typeof Convert);
