8c1604239f895afe0cb0352b462a88a727c7e7da
AkshayKumar2211 <taara0361@gmail.com>
2025-05-22 22:42:24 +0530

    ConsoleTable

---
+++ table.js
@@ -0,0 +1,8 @@
+let a=1;
+let name="Akshay Kumar";
+let state="Himachal Pradesh";
+let journey="Learnig Coding";
+
+console.table([a,name,state,journey]);
+
+//console.table is used to display output in tabular form.
