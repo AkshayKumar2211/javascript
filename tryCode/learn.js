88fdab670eeb7dec0e05b472576938875a2587d8
AkshayKumar2211 <taara0361@gmail.com>
2025-05-23 15:27:09 +0530

    change

--- tryCode/learn.js
+++ tryCode/learn.js
@@ -12,3 +12,19 @@ console.log(typeof Convert);
 Convert=String(Convert);
 
 console.log(typeof Convert);
+
+
+//in javascript non primitive are assigned memory in the heap that it can change permantly if reference to another variable
+
+let user={
+    name:"Akshay",
+    class: 10,
+
+};
+
+console.log(user);
+let user2=user;
+user2.name="Anaya";
+console.log(user);
+console.log(user2);
+
