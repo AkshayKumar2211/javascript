df99cbb4a4d2e10e41d53076ebedc6c965be54ce
AkshayKumar2211 <taara0361@gmail.com>
2025-05-24 10:22:58 +0530

    colorChanger

---
+++ ColorChanger/logic.js
@@ -0,0 +1,10 @@
+const spanbutton=document.querySelectorAll("#col");
+let color=["white","black","blue","red"];
+
+let i=0;
+spanbutton.forEach(button=>{
+   button.addEventListener("click",()=>{
+       document.body.style.backgroundColor=button.textContent;
+       button.style.color=button.textContent;
+    });
+});
--- Dom/logic.js
+++ Dom/logic.js
@@ -1,3 +1,6 @@
 document.querySelector("button").addEventListener("click", function () {
     document.getElementById("heading").textContent = "Project in Progress!";
 });
+
+
+//learn about event listene getelementby id ,by queryseletor and many more
